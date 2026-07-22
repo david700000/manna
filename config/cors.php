280fe24ae7d61263d8b12b141827c0abdb8ecdfa
david700000 <david07israel@gmail.com>
2026-07-22 16:01:53 +0100

    fix: allow all origins in CORS for local frontend testing

--- config/cors.php
+++ config/cors.php
@@ -19,13 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_filter([
-        env('FRONTEND_URL'),
-        env('APP_URL'),
-        'http://localhost:3000', // local dev
-    ]),
+    'allowed_origins' => [],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['.*'],
 
     'allowed_headers' => ['*'],
 
