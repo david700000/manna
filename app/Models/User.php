a32de754663a08f190929be192a82c32dc0b31bc
david700000 <david07israel@gmail.com>
2026-08-07 10:25:56 +0100

    Fix boolean cast for must_change_password in Postgres

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,14 +25,11 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-            'must_change_password' => 'boolean',
-        ];
-    }
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+        'must_change_password' => 'boolean',
+    ];
 
     public function orders()
     {
